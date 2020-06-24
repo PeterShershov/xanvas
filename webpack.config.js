f97506f6ee1d2fe0a0e86eac203c961276c550d8
Peter Shershov <stelaviv@gmail.com>
2020-06-25 01:50:45 +0300

    add demo

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,23 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-	module: {
-		rules: [
-			{
-				test: /\.tsx?$/,
-				loader: "@ts-tools/webpack-loader"
-			}
-		]
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: "src/assets/demo.html"
-		})
-	],
-	resolve: {
-		extensions: [".ts", ".tsx", ".mjs", ".js", ".json"]
-	}
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "@ts-tools/webpack-loader",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/assets/demo.html",
+    }),
+  ],
+  resolve: {
+    extensions: [".ts", ".tsx", ".mjs", ".js", ".json"],
+  },
+  devServer: {
+    open: true,
+  },
 };
