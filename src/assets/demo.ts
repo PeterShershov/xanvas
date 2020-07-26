f19da3374ca285dbf8e6ec56b0ebc27e83e3ea8e
Peter Shershov <peters@wix.com>
2020-07-26 14:21:17 +0300

    upgrade deps

--- src/assets/demo.ts
+++ src/assets/demo.ts
@@ -3,47 +3,23 @@ import { Layer } from "../layer";
 
 const xanvas = new Xanvas({ selector: "#root", width: 500, height: 500 });
 
-const firstLayer = new Layer({
-  name: "first",
-  draw: (context) => {
-    context.beginPath();
-    context.arc(200, 500 / 2, 90, Math.PI * 2, 0);
+let color = 0,
+  x = 0,
+  y = 0;
 
-    context.fillStyle = `tomato`;
-    context.fill();
-  },
-});
+for (let i = 0; i < 50; i++) {
+  const layer = new Layer({
+    name: "first" + i,
+    draw: (context) => {
+      context.beginPath();
+      context.arc(x++, y++, 50, Math.PI * 2, 0);
 
-const secondLayer = new Layer({
-  name: "second",
-  draw: (context) => {
-    context.beginPath();
-    context.arc(500 / 2, 500 / 2, 90, Math.PI * 2, 0);
+      context.fillStyle = `tomato`;
+      context.fill();
+    },
+  });
 
-    context.strokeStyle = `rgb(100, 255, 90)`;
-    context.lineWidth = 5;
-    context.stroke();
-  },
-});
-
-const thirdLayer = new Layer({
-  name: "third",
-  draw: (context) => {
-    context.beginPath();
-    context.arc(500 / 1.8, 500 / 2, 90, Math.PI * 2, 0);
-
-    context.fillStyle = `rgba(221, 222, 12, 0.3)`;
-    context.fill();
-  },
-});
-xanvas.addLayer(secondLayer);
-xanvas.addLayer(firstLayer);
-xanvas.addLayer(thirdLayer);
-
-let isHidden = false;
-setInterval(() => {
-  isHidden ? xanvas.showLayer(secondLayer) : xanvas.hideLayer(secondLayer);
-  isHidden = !isHidden;
-}, 1000);
+  xanvas.addLayer(layer);
+}
 
 xanvas.draw();
